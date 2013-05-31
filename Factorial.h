a8397a3ee4f8e83fce46ccdeb98b5bd79cb1cfcf
Oguzhan Gungor <cansingungor@gmail.com>
2013-06-01 09:26:51 +1000

    header file added

---
+++ Factorial.h
@@ -0,0 +1,14 @@
+//
+//  Factorial.h
+//  Factorial_C
+//
+//  Created by Oguzhan Gungor on 24/04/13.
+//  Copyright (c) 2013 Oguzhan Gungor. All rights reserved.
+//
+
+#ifndef __Factorial_C__Factorial__
+#define __Factorial_C__Factorial__
+
+int factorial(int n);
+
+#endif /* defined(__Factorial_C__Factorial__) */
